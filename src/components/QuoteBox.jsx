60618152cb3f3cfd2e8cf83a68b6c57b6bd98932
Yashkumbhar1518 <136221314+Yashkumbhar1518@users.noreply.github.com>
2025-08-07 12:13:33 +0530

    Update QuoteBox.jsx

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+er import React from 'react';
 
 function QuoteBox({ quote, getNewQuote }) {
   return (
     <div className="quote-box">
       <p className="quote">“{quote.text}”</p>
       <p className="author">— {quote.author}</p>
-      <button onClick={getNewQuote}>New Quote</button>
+      <button onClick={getNewQuote}>Enter New Quote</button>
     </div>
   );
 }
